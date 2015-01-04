c92167f8b3a842ee49937e927bcd9a70568056a4
beejeebus <justin.p.randell@gmail.com>
2015-01-04 13:25:38 -0500

    Move api.go to karmatrain.go.

--- karmatrain/karmatrain.go
+++ karmatrain/karmatrain.go
@@ -1,5 +1,19 @@
 package karmatrain
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
-var thing = fmt.Println
+var activeTopics = &ActiveTopics{
+	register_topic: make(chan string),
+	topics:         make(map[string]*TopicHub),
+}
+
+func WebsocketApiTopicConsumer(response http.ResponseWriter, request *http.Request) {
+	c, err := NewWebsocket(response, request)
+	if err != nil {
+		return
+	}
+	activeTopics.RegisterConnectionForTopic(mux.Vars(request)["topic"], c)
+}
